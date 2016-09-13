b100fdb0d0f7c1014a3d70805bcb491d3facc823
lijiangHou <lijiang.hou@gmail.com>
2016-09-13 12:08:46 +0800

    修复deploy manager 因为权限问题crash bug

--- app/src/main/java/com/common/app/api/ApiConstants.java
+++ app/src/main/java/com/common/app/api/ApiConstants.java
@@ -1,5 +1,7 @@
 package com.common.app.api;
 
+import com.common.app.base.manager.DeployManager;
+
 /**
  * Created by houlijiang on 16/1/23.
  */
@@ -7,9 +9,24 @@ public class ApiConstants {
 
     public static final int API_LIST_FIRST_PAGE = 1;
 
-    public static final String AUTH_HOST_TEST = "http://test-i.houlijiang.com/";
-    public static final String AUTH_HOST_BETA = "http://beta-i.houlijiang.com/";
-    public static final String AUTH_HOST_ONLINE = "http://i.houlijiang.com/";
+    public static final String API_HOST;
+    static {
+        switch (DeployManager.getEnvironmentType()) {
+            case TYPE_TEST: {
+                API_HOST = "http://test-i.houlijiang.com/api/";
+                break;
+            }
+            case TYPE_BETA: {
+                API_HOST = "http://beta-i.houlijiang.com/api/";
+                break;
+            }
+            case TYPE_ONLINE:
+            default: {
+                API_HOST = "http://i.houlijiang.com/api/";
+                break;
+            }
+        }
+    }
 
     public static final String API_LOGIN = "app/auth/doLogin.do";
 }
