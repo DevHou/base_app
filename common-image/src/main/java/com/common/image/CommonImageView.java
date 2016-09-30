e9b8246dd19bad9c0ba5699f6dc565b6225b34b7
lijiangHou <lijiang.hou@gmail.com>
2016-09-30 15:55:29 +0800

    修改图片库 简化切换是修改的东西

--- common-image/src/main/java/com/common/image/CommonImageView.java
+++ common-image/src/main/java/com/common/image/CommonImageView.java
@@ -1,25 +1,109 @@
 package com.common.image;
 
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Bitmap;
 import android.util.AttributeSet;
+import android.widget.ImageView;
+
+import com.common.utils.AppLog;
 
 /**
  * Created by houlijiang on 15/4/2.
  * 
  * 只是通过继承不同库的具体实现切换
  */
-public class CommonImageView extends com.common.image.glide.CommonImageView {
+public class CommonImageView extends ImageView {
+
+    private static final String TAG = CommonImageView.class.getSimpleName();
 
     public CommonImageView(Context context) {
-        super(context);
+        this(context, null, 0);
     }
 
     public CommonImageView(Context context, AttributeSet attrs) {
-        super(context, attrs);
+        this(context, attrs, 0);
     }
 
-    public CommonImageView(Context context, AttributeSet attrs, int defStyle) {
-        super(context, attrs, defStyle);
+    public CommonImageView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context, attrs);
+    }
+
+    private void init(Context context, AttributeSet attrs) {
+        if (attrs != null) {
+            TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.CommonImageView);
+            int attributeIds[] = { android.R.attr.src };
+            TypedArray b = context.obtainStyledAttributes(attrs, attributeIds);
+            try {
+                int imageId = a.getResourceId(R.styleable.CommonImageView_imageSrc, 0);
+                int imageScaleTypeInt = a.getInt(R.styleable.CommonImageView_imageScaleType, -1);
+
+                if (imageScaleTypeInt != -1) {
+                    ImageView.ScaleType scaleType = ImageView.ScaleType.CENTER;
+                    if (imageScaleTypeInt == ImageScaleType.center.value()) {
+                        scaleType = ImageView.ScaleType.CENTER;
+                    } else if (imageScaleTypeInt == ImageScaleType.centerCrop.value()) {
+                        scaleType = ImageView.ScaleType.CENTER_CROP;
+                    } else if (imageScaleTypeInt == ImageScaleType.centerInside.value()) {
+                        scaleType = ImageView.ScaleType.CENTER_INSIDE;
+                    } else if (imageScaleTypeInt == ImageScaleType.fitCenter.value()) {
+                        scaleType = ImageView.ScaleType.FIT_CENTER;
+                    } else if (imageScaleTypeInt == ImageScaleType.fitEnd.value()) {
+                        scaleType = ImageView.ScaleType.FIT_END;
+                    } else if (imageScaleTypeInt == ImageScaleType.fitStart.value()) {
+                        scaleType = ImageView.ScaleType.FIT_START;
+                    } else if (imageScaleTypeInt == ImageScaleType.fitXY.value()) {
+                        scaleType = ImageView.ScaleType.FIT_XY;
+                    }
+                    setScaleType(scaleType);
+                }
+
+                int resourceId = b.getResourceId(0, 0);
+                if (resourceId != 0 && imageId == 0) {
+                    imageId = resourceId;
+                }
+
+                loadImage(imageId);
+            } catch (Exception e) {
+                AppLog.e(TAG, "common image get attr error, e:" + e.getLocalizedMessage());
+            } finally {
+                a.recycle();
+                b.recycle();
+            }
+        }
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+    }
+
+    public void onViewRecycled() {
+        setImageResource(0);
+    }
+
+    /**
+     * 加载资源图片
+     *
+     * @param resId 资源ID
+     */
+    protected void loadImage(int resId) {
+        setImageResource(resId);
+    }
+
+    /**
+     * 子类可以重写这个方法，返回合适的对象
+     */
+    public com.bumptech.glide.load.Transformation<Bitmap> createTransformation() {
+        return null;
+    }
+
+    /**
+     * 子类可以重写这个方法，返回合适的对象
+     */
+    public com.squareup.picasso.Transformation getTransform() {
+        return null;
     }
 
     /**
