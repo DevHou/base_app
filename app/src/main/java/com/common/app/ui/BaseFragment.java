1386d67c1d7e732f93028074f4f89cb0f4715277
SeanHou <houlijiang@evermarker.net>
2017-03-24 17:36:51 +0800

    升级依赖库版本

--- app/src/main/java/com/common/app/ui/BaseFragment.java
+++ app/src/main/java/com/common/app/ui/BaseFragment.java
@@ -1,11 +1,41 @@
 package com.common.app.ui;
 
 import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 
 /**
  * Created by houlijiang on 16/1/25.
  * 
  * 基类
+ *
+ * STATE_SAVE_IS_HIDDEN使用来保存fragment的显示状态的，
+ * 因为默认它没保存这个，当异常重启后，所有fragment都是默认的mHidden=false，所以就会导致fragment重叠
  */
 public class BaseFragment extends Fragment {
+
+    private static final String STATE_SAVE_IS_HIDDEN = "STATE_SAVE_IS_HIDDEN";
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (savedInstanceState != null) {
+            boolean isSupportHidden = savedInstanceState.getBoolean(STATE_SAVE_IS_HIDDEN);
+            FragmentTransaction ft = getFragmentManager().beginTransaction();
+            if (isSupportHidden) {
+                ft.hide(this);
+            } else {
+                ft.show(this);
+            }
+            ft.commit();
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        outState.putBoolean(STATE_SAVE_IS_HIDDEN, isHidden());
+    }
+
 }
